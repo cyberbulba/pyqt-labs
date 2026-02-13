51306dfbb0abd675eca40d9d15294f8a60226690
Your Name <you@example.com>
2026-02-13 15:05:12 +0300

    попытки сделать нормальный label

--- basics/main.py
+++ basics/main.py
@@ -1,26 +1,42 @@
 import sys
 
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
 
-def create_window(screen):
-    screen_geometry = screen.availableGeometry()
 
-    # width = screen_geometry.width()
+class MyWindow(QMainWindow):
+    def __init__(self, height, screen_geometry):
+        super().__init__()
+
+        self.resize(height, height)
+
+        window_geometry = self.frameGeometry()
+        window_geometry.moveCenter(screen_geometry.center())
+        self.move(window_geometry.topLeft())
+
+
+def main():
+    app = QApplication(sys.argv)
+
+    screen = QApplication.primaryScreen()
+    screen_geometry = screen.availableGeometry()
     height = screen_geometry.height()
 
-    window = QMainWindow()
-    window.resize(height, height)
+    window = MyWindow(height, screen_geometry)
+    label = QLabel(window)
+    label.setText("bear" * 100)
 
-    window_geometry = window.frameGeometry()
-    window_geometry.moveCenter(screen_geometry.center())
-    window.move(window_geometry.topLeft())
+    font = QFont("Arial", 12)
+    font = QFont("Times New Roman", 14)
+    font.setItalic(True)
 
-    return window
+    label.setFont(font)
 
-app = QApplication(sys.argv)
-screen = QApplication.primaryScreen()
+    window.show()
+    label.show()
+    app.exec()
 
-window = create_window(screen)
 
-window.show()
-app.exec()
+if __name__ == "__main__":
+    main()
