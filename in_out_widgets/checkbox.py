ba61051b31aa78ac3a2163c4ac2cf74f191bb894
Your Name <you@example.com>
2026-02-19 10:51:47 +0300

    3-2

--- in_out_widgets/checkbox.py
+++ in_out_widgets/checkbox.py
@@ -1,6 +1,7 @@
 import sys
 
-from PySide6.QtWidgets import QPushButton, QButtonGroup, QRadioButton, QTextEdit, QCheckBox, QSlider, QSpinBox
+from PySide6.QtWidgets import QPushButton, QButtonGroup, QRadioButton, QTextEdit, QCheckBox, QSlider, QSpinBox, \
+    QLineEdit
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
@@ -40,13 +41,13 @@ class MyWindow(QMainWindow):
         self.layout.addLayout(self.checkbox_layout)
 
         self.cost_arr = [("виноград", 80), ("сливы", 70), ("яблоки зелёные", 30),
-               ("бананы", 50), ("груши", 60)]
+                         ("бананы", 50), ("груши", 60)]
 
         for i in range(len(self.cost_arr)):
             check_box = QCheckBox(self.cost_arr[i][0] + ' - ' + str(self.cost_arr[i][1]) + ' руб/кг')
 
             spinBox = QSpinBox()
-            spinBox.setRange(0, 30)
+            spinBox.setRange(1, 100)
             spinBox.setSuffix(' кг')
 
             self.checkbox_layout.addWidget(check_box)
@@ -58,18 +59,33 @@ class MyWindow(QMainWindow):
         self.label_res = QLabel()
         self.layout.addWidget(self.label_res)
 
+        self.text_res = QTextEdit()
+        self.layout.addWidget(self.text_res)
+
         button.clicked.connect(self.get_cost)
 
     def handle_checkboxes(self):
         self.cost = 0
+        text = ' '
+        self.text_res.clear()
         for i in range(0, self.checkbox_layout.count(), 2):
             checkbox = self.checkbox_layout.itemAt(i).widget()
             spinbox = self.checkbox_layout.itemAt(i + 1).widget()
 
+            font = QFont()
+            checkbox.setFont(font)
+
             price = self.cost_arr[i // 2][1]
+            name = self.cost_arr[i // 2][0]
 
             if checkbox.isChecked():
                 self.cost += price * spinbox.value()
+                font = QFont()
+                font.setBold(True)
+                checkbox.setFont(font)
+                text += f'{name}: {price} * {spinbox.value()} = {price * spinbox.value()} руб\n '
+
+        self.text_res.setText(text)
 
     @Slot()
     def get_cost(self):
