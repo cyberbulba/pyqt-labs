d2e022062e4225d9c5c4a5e211b5ddf59b8a3d2e
Your Name <you@example.com>
2026-03-20 19:53:56 +0300

    6-3

--- dialog/wizard.py
+++ dialog/wizard.py
@@ -2,7 +2,7 @@ import sys
 
 from PySide6.QtCore import QRegularExpression
 from PySide6.QtGui import QRegularExpressionValidator
-from PySide6.QtWidgets import QPushButton, QDialog, QCheckBox, QWizard, QWizardPage, QLineEdit
+from PySide6.QtWidgets import QPushButton, QDialog, QCheckBox, QWizard, QWizardPage, QLineEdit, QTextEdit
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
 
 
@@ -36,14 +36,17 @@ class FIOPage(QWizardPage):
         self.family = QLineEdit()
         self.family.setPlaceholderText("Фамилия")
         self.family.setValidator(self.validator_names)
+        self.family.textChanged.connect(self.check_complete)
 
         self.name = QLineEdit()
         self.name.setPlaceholderText("Имя")
         self.name.setValidator(self.validator_names)
+        self.name.textChanged.connect(self.check_complete)
 
         self.fathername = QLineEdit()
         self.fathername.setPlaceholderText("Отчество")
         self.fathername.setValidator(self.validator_names)
+        self.fathername.textChanged.connect(self.check_complete)
 
         layout = QVBoxLayout(self)
         layout.addWidget(label)
@@ -51,6 +54,37 @@ class FIOPage(QWizardPage):
         layout.addWidget(self.name)
         layout.addWidget(self.fathername)
 
+        self.error_text = QTextEdit()
+        self.error_text.setReadOnly(True)
+        layout.addWidget(self.error_text)
+
+    def isComplete(self):
+        error_string = ""
+
+        state_family, _, _ = self.validator_names.validate(self.family.text(), 0)
+        state_name, _, _ = self.validator_names.validate(self.name.text(), 0)
+        state_fathername, _, _ = self.validator_names.validate(self.fathername.text(), 0)
+
+        if state_family != QRegularExpressionValidator.State.Acceptable:
+            error_string += "Неправильная фамилия!\n"
+
+        if state_name != QRegularExpressionValidator.State.Acceptable:
+            error_string += "Неправильное имя!\n"
+
+        if state_fathername != QRegularExpressionValidator.State.Acceptable:
+            error_string += "Неправильное отчество!\n"
+
+        if not error_string:
+            self.error_text.setText("Все данные корректны")
+        else:
+            self.error_text.setText(error_string)
+
+        return (state_family == QRegularExpressionValidator.State.Acceptable and
+                state_name == QRegularExpressionValidator.State.Acceptable and
+                state_fathername == QRegularExpressionValidator.State.Acceptable)
+
+    def check_complete(self):
+        self.completeChanged.emit()
 
 class CheckboxPage(QWizardPage):
     def __init__(self):
