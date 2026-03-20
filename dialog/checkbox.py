503f09698333b6c3e848361016c5dc1ba18c0ca6
Your Name <you@example.com>
2026-03-20 17:37:24 +0300

    5-2

--- dialog/checkbox.py
+++ dialog/checkbox.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QPushButton, QDialog
+from PySide6.QtWidgets import QPushButton, QDialog, QCheckBox
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
 from PySide6.QtCore import Slot
 
@@ -13,7 +13,20 @@ class MyDialog(QDialog):
 
         layout = QVBoxLayout()
         layout.addWidget(QLabel("Вы принимаете соглашение?"))
+        self.checkbox = QCheckBox("Соглашаюсь")
+        layout.addWidget(self.checkbox)
+        self.button = QPushButton("ОК")
+        layout.addWidget(self.button)
+
         self.setLayout(layout)
+
+        self.button.clicked.connect(self.close_dialog)
+
+    def check_box_clicked(self):
+        return self.checkbox.isChecked()
+
+    def close_dialog(self):
+        self.close()
 
 
 class MyWindow(QMainWindow):
@@ -43,13 +56,24 @@ class MyWindow(QMainWindow):
         button = QPushButton("Открыть диалоговое окно")
         self.layout.addWidget(button)
 
+        self.res_label = QLabel()
+        self.layout.addWidget(self.res_label)
+
         self.dialog = MyDialog()
 
         button.clicked.connect(self.open_dialog)
+        self.dialog.finished.connect(self.write_agreement)
 
     def open_dialog(self):
         self.dialog.exec()
 
+    def write_agreement(self):
+        if self.dialog.check_box_clicked():
+            text = "чекбокс выбран"
+        else:
+            text = "чекбокс не выбран"
+
+        self.res_label.setText(text)
 
 
 def main():
