fe7b6792c4bf2e6104f80fad64813376fb040a92
Your Name <you@example.com>
2026-03-29 19:54:46 +0300

    7-2

--- dialog/Note.py
+++ dialog/Note.py
@@ -1,13 +1,13 @@
 class Note:
     def __init__(self, text, date):
-        self.text = text
-        self.date = date
+        self.__text = text
+        self.__date = date
 
     def get_text(self):
-        return self.text
+        return self.__text
 
     def get_date(self):
-        return self.date
+        return self.__date
 
     def __str__(self):
-        return f'{self.text} {self.date.toString("dd.MM.yyyy")}'
+        return f'{self.__text} {self.__date.toString("dd.MM.yyyy")}'
