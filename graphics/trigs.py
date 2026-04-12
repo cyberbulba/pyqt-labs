2c31da94fae1f6c4af41dad6402a1ba9fe1e3f59
Your Name <you@example.com>
2026-04-12 08:52:11 +0300

    9-1

--- graphics/trigs.py
+++ graphics/trigs.py
@@ -1,8 +1,11 @@
 import sys
+from math import sin, pi, cos
 
+from PySide6.QtCharts import QChart, QChartView, QLineSeries, QSplineSeries
 from PySide6.QtWidgets import QPushButton
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot, QPointF
+import pyqtgraph as pg
 
 
 class MyWindow(QMainWindow):
@@ -11,12 +14,12 @@ class MyWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Кнопка")
+        self.setWindowTitle("Графики тригонометрии")
 
         screen = QApplication.primaryScreen()
         screen_geometry = screen.availableGeometry()  # узнаём размеры экрана и устанавливаем окно
 
-        self.height = int(screen_geometry.height() * 0.4)
+        self.height = int(screen_geometry.height() * 0.6)
 
         self.central_widget = QWidget()
         self.central_widget.setFixedSize(self.height, self.height)
@@ -27,23 +30,46 @@ class MyWindow(QMainWindow):
         self.move(window_geometry.topLeft())
 
         self.layout = QVBoxLayout(self.central_widget)
+        self.layout.addWidget(QLabel("График синуса"))
 
-        self.label = QLabel()
-        self.layout.addWidget(self.label)
-        self.label.setText("Кнопка не нажата!")
+        self.chart_1 = QChart()
+        self.chart_1.legend().hide()
 
-        button = QPushButton("Нажми на меня!")
-        self.layout.addWidget(button)
-        button.pressed.connect(self.swap_label_pressed)
-        button.released.connect(self.swap_label_released)
+        self.series_1 = QSplineSeries()
+        self.create_series_1()
 
-    @Slot()
-    def swap_label_pressed(self):
-        self.label.setText("Кнопка нажата!")
+        self.chart_1.addSeries(self.series_1)
+        self.chart_1.createDefaultAxes()
 
-    @Slot()
-    def swap_label_released(self):
-        self.label.setText("Кнопка отпущена!")
+        self.chart_view_1 = QChartView(self.chart_1)
+        self.layout.addWidget(self.chart_view_1)
+
+        self.layout.addWidget(QLabel("График косинуса"))
+
+        self.chart_2 = QChart()
+        self.chart_2.legend().hide()
+
+        self.series_2 = QSplineSeries()
+        self.create_series_2()
+
+        self.chart_2.addSeries(self.series_2)
+        self.chart_2.createDefaultAxes()
+
+        self.chart_view_2 = QChartView(self.chart_2)
+        self.layout.addWidget(self.chart_view_2)
+
+    def create_series_1(self):
+        x = -5
+        for i in range(100):
+            self.series_1.append(QPointF(x, sin(x)))
+            x += 0.1
+
+    def create_series_2(self):
+        x = -5
+        for i in range(100):
+            self.series_2.append(QPointF(x, cos(x)))
+            x += 0.1
+
 
 
 def main():
