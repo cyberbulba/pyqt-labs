41c3993ca5a8f97ad50b6c1899a20603023e3179
Your Name <you@example.com>
2026-04-13 16:12:32 +0300

    10-1

--- graphics/trees.py
+++ graphics/trees.py
@@ -2,14 +2,14 @@ import csv
 import sys
 from math import sin, pi, cos
 
-import numpy as np
-from PySide6.QtCharts import QChart, QChartView, QLineSeries, QSplineSeries
-from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QPushButton
+import pandas as pd
+from PySide6.QtCharts import QChart, QChartView, QLineSeries, QSplineSeries, QValueAxis, QBarCategoryAxis, QBarSet, \
+    QBarSeries, QScatterSeries
+from PySide6.QtGui import QPainter
+from PySide6.QtWidgets import QPushButton, QHBoxLayout
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
-from PySide6.QtCore import Slot, QPointF, Qt, QTimer
+from PySide6.QtCore import Slot, QPointF, Qt
 import pyqtgraph as pg
-import pyqtgraph.opengl as gl
 
 
 class MyWindow(QMainWindow):
@@ -18,7 +18,7 @@ class MyWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Точечная диаграмма о деревьях")
+        self.setWindowTitle("Точечные диаграммы о характеристиках деревьев")
 
         screen = QApplication.primaryScreen()
         screen_geometry = screen.availableGeometry()  # узнаём размеры экрана и устанавливаем окно
@@ -26,68 +26,107 @@ class MyWindow(QMainWindow):
         self.height = int(screen_geometry.height() * 0.6)
 
         self.central_widget = QWidget()
-        self.central_widget.setFixedSize(self.height, self.height)
+        self.central_widget.setFixedSize(self.height * 2, self.height)
         self.setCentralWidget(self.central_widget)
 
         window_geometry = self.frameGeometry()
         window_geometry.moveCenter(screen_geometry.center())  # перемещаем окно в центр
         self.move(window_geometry.topLeft())
 
-        self.layout = QVBoxLayout(self.central_widget)
+        self.layout = QHBoxLayout(self.central_widget)
 
-        self.view = gl.GLViewWidget()
+        df = pd.read_csv("trees.csv")
 
-        self.layout.addWidget(self.view)
+        self.create_first_chart(df)
+        self.create_second_chart(df)
+        self.create_third_chart(df)
 
-        axis = gl.GLAxisItem()
-        axis.setSize(x=100, y=100, z=100)
+    def create_first_chart(self, df):
+        series = QScatterSeries()
 
-        self.view.addItem(axis)
+        for i in range(len(df["Girth"].tolist())):
+            series.append(df["Girth"].tolist()[i], df["Height"].tolist()[i])
 
-        self.view.setCameraPosition(distance=1000, elevation=30, azimuth=45)
+        chart = QChart()
+        chart.legend().setVisible(False)
 
-        trees_arr_dicts = parse_csv('trees.csv')
-        points = get_points(trees_arr_dicts)
+        chart.addSeries(series)
+        chart.setTitle("Зависимость обхвата от высоты дерева")
+        chart.setAnimationOptions(QChart.SeriesAnimations)
 
-        point = gl.GLScatterPlotItem(
-            pos=np.array(points),
-            size=1,
-            color=(1, 0, 0, 1),
-            pxMode=False
-        )
-        self.view.addItem(point)
-        QTimer.singleShot(100, self.add_labels)
+        axis_x = QValueAxis()
+        axis_x.setTitleText("Обхват")
+        axis_x.setRange(0, 50)
+        chart.addAxis(axis_x, Qt.AlignBottom)
+        series.attachAxis(axis_x)
 
-    def add_labels(self):
-        """Добавляем белые подписи осей"""
-        labels_config = [
-            ("Girth (X)", self.height * 0.8, self.height * 0.9, "yellow"),
-            ("Height (Y)", 10, self.height * 0.1, "green"),
-            ("Volume (Z)", 10, self.height * 0.9, "blue"),
-        ]
+        axis_y = QValueAxis()
+        axis_y.setRange(0, 100)
+        axis_y.setTitleText("Высота")
+        chart.addAxis(axis_y, Qt.AlignLeft)
+        series.attachAxis(axis_y)
 
-        for text, x, y, color in labels_config:
-            lbl = QLabel(text, self.view)
-            lbl.setStyleSheet(f"color: {color}; font: bold 14px Arial; background: transparent;")
-            lbl.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents, True)
-            lbl.move(x, y)
-            lbl.show()
+        _chart_view = QChartView(chart)
+        _chart_view.setRenderHint(QPainter.Antialiasing)
+        self.layout.addWidget(_chart_view)
 
+    def create_second_chart(self, df):
+        series = QScatterSeries()
 
-def parse_csv(csv_file):
-    try:
-        with open(csv_file, 'r', encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            return list(reader)
-    except FileNotFoundError:
-        return []
+        for i in range(len(df["Volume"].tolist())):
+            series.append(df["Volume"].tolist()[i], df["Height"].tolist()[i])
 
-def get_points(trees_arr):
-    tree_arr = []
-    for tree in trees_arr:
-        tree_arr.append([float(tree['Girth']), float(tree['Height']), float(tree['Volume'])])
+        chart = QChart()
+        chart.legend().setVisible(False)
 
-    return tree_arr
+        chart.addSeries(series)
+        chart.setTitle("Зависимость объёма от высоты дерева")
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+
+        axis_x = QValueAxis()
+        axis_x.setTitleText("Объём")
+        axis_x.setRange(0, 100)
+        chart.addAxis(axis_x, Qt.AlignBottom)
+        series.attachAxis(axis_x)
+
+        axis_y = QValueAxis()
+        axis_y.setRange(0, 100)
+        axis_y.setTitleText("Высота")
+        chart.addAxis(axis_y, Qt.AlignLeft)
+        series.attachAxis(axis_y)
+
+        _chart_view = QChartView(chart)
+        _chart_view.setRenderHint(QPainter.Antialiasing)
+        self.layout.addWidget(_chart_view)
+
+    def create_third_chart(self, df):
+        series = QScatterSeries()
+
+        for i in range(len(df["Girth"].tolist())):
+            series.append(df["Girth"].tolist()[i], df["Volume"].tolist()[i])
+
+        chart = QChart()
+        chart.legend().setVisible(False)
+
+        chart.addSeries(series)
+        chart.setTitle("Зависимость обхвата от объёма дерева")
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+
+        axis_x = QValueAxis()
+        axis_x.setTitleText("Обхват")
+        axis_x.setRange(0, 50)
+        chart.addAxis(axis_x, Qt.AlignBottom)
+        series.attachAxis(axis_x)
+
+        axis_y = QValueAxis()
+        axis_y.setRange(0, 100)
+        axis_y.setTitleText("Объём")
+        chart.addAxis(axis_y, Qt.AlignLeft)
+        series.attachAxis(axis_y)
+
+        _chart_view = QChartView(chart)
+        _chart_view.setRenderHint(QPainter.Antialiasing)
+        self.layout.addWidget(_chart_view)
 
 
 def main():
