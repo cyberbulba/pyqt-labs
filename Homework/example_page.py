80850d65671799765adaf44c9e29e9fcdf191a62
Your Name <you@example.com>
2026-04-20 08:46:59 +0300

    добавил инфо по сложным примерам

---
+++ Homework/example_page.py
@@ -0,0 +1,77 @@
+import random
+import sys
+
+from PySide6.QtGui import QRegularExpressionValidator
+from PySide6.QtWidgets import QGridLayout, QTextEdit, QWizard, QMessageBox, QLineEdit, QWizardPage, QCheckBox
+from PySide6.QtWidgets import QPushButton, QDialog, QButtonGroup, QRadioButton
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
+from PySide6.QtCore import Slot, QRegularExpression, Qt
+from generate_examples import RandomExample1
+
+
+class ExamplePage(QWizardPage):
+    def __init__(self, action=None, sign=1):
+        super().__init__()
+        self.__answered = False
+        label = QLabel("Решите пример:")
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(label)
+
+        if action is None:
+            self.example = RandomExample1(sign=sign)
+        else:
+            self.example = RandomExample1(action, sign=sign)
+
+        self.radio_group = QButtonGroup()
+        arr = set()
+        arr.add(self.example.get_result())
+        while len(arr) != 4:
+            arr.add(random.randint(-100, 100))
+
+        arr = list(arr)
+        random.shuffle(arr)
+
+        self.label = QLabel(self.example.get_without_answer())
+        layout.addWidget(self.label)
+
+        for i in range(len(arr)):
+            button = QRadioButton(str(arr[i]))
+            self.radio_group.addButton(button, id=i)
+            layout.addWidget(button)
+
+        self.radio_group.buttonClicked.connect(self.check_complete)
+
+        self.__res = 0
+
+    def isComplete(self):
+        if self.radio_group.checkedButton():
+            self.__answered = True
+            ans = float(self.radio_group.checkedButton().text())
+
+            if ans == self.example.get_result():
+                self.__res = 1
+            else:
+                self.__res = 0
+
+        return self.radio_group.checkedButton() is not None
+
+    def get_action(self):
+        return self.example.get_action()
+
+    def get_res(self):
+        if self.__res:
+            return self.__res
+        return 0
+
+    def get_error(self):
+        if self.__res == 0:
+            return self.example.get_action()
+        return None
+
+    def has_answered(self):
+        return self.__answered
+
+    @Slot()
+    def check_complete(self):
+        self.completeChanged.emit()
--- Homework/wizard_1_lvl.py
+++ Homework/wizard_1_lvl.py
@@ -7,12 +7,13 @@ from PySide6.QtWidgets import QPushButton, QDialog, QButtonGroup, QRadioButton
 from PySide6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QVBoxLayout
 from PySide6.QtCore import Slot, QRegularExpression, Qt
 from generate_examples import RandomExample1
+from example_page import ExamplePage
 
 
 class MyWizard(QWizard):
     def __init__(self, level=1):
         super().__init__()
-        self.__page_num = 5
+        self.__page_num = 2
         self.__page_count = 1
         self.__add_count_plus = 0
         self.__add_count_minus = 0
@@ -38,7 +39,7 @@ class MyWizard(QWizard):
         self.__pages.append(page)
 
     def reset_wizard(self):
-        self.__page_num = 5
+        self.__page_num = 2
         self.__page_count = 1
         self.__add_count_plus = 0
         self.__add_count_minus = 0
@@ -148,71 +149,3 @@ class MyWizard(QWizard):
         return sum(list(filter(lambda p: p == 1, self.__statistic))), len(
             list(filter(lambda p: p == 0, self.__statistic))), self.__errors.count("+"), self.__errors.count(
             "-"), self.__errors.count("*"), self.__errors.count("/")
-
-
-class ExamplePage(QWizardPage):
-    def __init__(self, action=None, sign=1):
-        super().__init__()
-        self.__answered = False
-        label = QLabel("Решите пример:")
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(label)
-
-        if action is None:
-            self.example = RandomExample1(sign=sign)
-        else:
-            self.example = RandomExample1(action, sign=sign)
-
-        self.radio_group = QButtonGroup()
-        arr = set()
-        arr.add(self.example.get_result())
-        while len(arr) != 4:
-            arr.add(random.randint(-100, 100))
-
-        arr = list(arr)
-        random.shuffle(arr)
-
-        self.label = QLabel(self.example.get_without_answer())
-        layout.addWidget(self.label)
-
-        for i in range(len(arr)):
-            button = QRadioButton(str(arr[i]))
-            self.radio_group.addButton(button, id=i)
-            layout.addWidget(button)
-
-        self.radio_group.buttonClicked.connect(self.check_complete)
-
-        self.__res = 0
-
-    def isComplete(self):
-        if self.radio_group.checkedButton():
-            self.__answered = True
-            ans = float(self.radio_group.checkedButton().text())
-
-            if ans == self.example.get_result():
-                self.__res = 1
-            else:
-                self.__res = 0
-
-        return self.radio_group.checkedButton() is not None
-
-    def get_action(self):
-        return self.example.get_action()
-
-    def get_res(self):
-        if self.__res:
-            return self.__res
-        return 0
-
-    def get_error(self):
-        if self.__res == 0:
-            return self.example.get_action()
-        return None
-
-    def has_answered(self):
-        return self.__answered
-
-    @Slot()
-    def check_complete(self):
-        self.completeChanged.emit()
